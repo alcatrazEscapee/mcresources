60fff5f5bdb6d68dcfca80715f4c73105c7d2707
EERussianguy <eerussianguy@gmail.com>
2022-08-24 16:12:33 -0400

    advancement update, crafting recipe shape checker

--- mcresources/resource_manager.py
+++ mcresources/resource_manager.py
@@ -206,6 +206,7 @@ class ResourceManager:
         :param group: The group.
         :param conditions: Any conditions for the recipe to be enabled.
         """
+        utils.validate_crafting_pattern(pattern)
         res = utils.resource_location(self.domain, name_parts)
         self.write((*self.resource_dir, 'data', res.domain, 'recipes', res.path), {
             'type': 'minecraft:crafting_shaped',
@@ -253,12 +254,14 @@ class ResourceManager:
         :param display: The display data. Inserted as is.
         :param parent: The parent advancement registry name
         :param criteria: The criteria for an advancement. Inserted as is.
-        :param requirements: The requirements. If None, will default to a list of all the requirements (OR'd together).
+        :param requirements: The requirements. If None or 'or', the criteria names will in one list. If 'and', the criteria will each get their own sublist, which requires all of them to be achieved.
         :param rewards: The rewards. Inserted as is.
         """
         res = utils.resource_location(self.domain, name_parts)
-        if requirements is None:
+        if requirements is None or requirements == 'or':
             requirements = [[k for k in criteria.keys()]]
+        elif requirements == 'and':
+            requirements = [[k] for k in criteria.keys()]
         self.write((*self.resource_dir, 'data', res.domain, 'advancements', res.path), {
             'parent': parent,
             'criteria': criteria,
